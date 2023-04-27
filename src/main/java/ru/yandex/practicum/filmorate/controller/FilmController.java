2a4995e5958b491235311cc7d5fc2ab39da46d77
Алишев Азамат <aalishevaa@yandex.ru>
2023-04-27 17:47:25 +0600

    ТЗ9/4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -16,8 +16,8 @@ import java.util.Map;
 public class FilmController {
     Map<Integer, Film> films = new HashMap<>();
     private int generatorId = 1;
-    private final int MAX_LENGTH = 200;
-    private final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
+    private final int LENGTH = 200;
+    private final LocalDate DATE = LocalDate.of(1895, 12, 28);
 
     @PostMapping("/films")
     public Film add(@RequestBody @Validated Film film) throws ValidationException {
@@ -25,13 +25,13 @@ public class FilmController {
             log.info("Название фильма пустое");
             throw new ValidationException("Название фильма пустое");
         }
-        if (film.getDescription().length() > MAX_LENGTH) {
-            log.info("Длина описания должна быть не больше " + MAX_LENGTH + " символов");
-            throw new ValidationException("Длина описания должна быть не больше " + MAX_LENGTH + " символов");
+        if (film.getDescription().length() > LENGTH) {
+            log.info("Длина описания должна быть не больше " + LENGTH + " символов");
+            throw new ValidationException("Длина описания должна быть не больше " + LENGTH + " символов");
         }
-        if (film.getReleaseDate().isBefore(MIN_DATE)) {
-            log.info("Дата публикации фильма раньше положенного, фильм должен быть опубликован не раньше чем - " + MIN_DATE);
-            throw new ValidationException("Дата публикации фильма раньше положенного, фильм должен быть опубликован не раньше чем - " + MIN_DATE);
+        if (film.getReleaseDate().isBefore(DATE)) {
+            log.info("Дата публикации фильма раньше положенного, фильм должен быть опубликован не раньше чем - " + DATE);
+            throw new ValidationException("Дата публикации фильма раньше положенного, фильм должен быть опубликован не раньше чем - " + DATE);
         }
         if (film.getDuration() < 0) {
             log.info("Продолжительность фильма не должна быть отрицательной");
